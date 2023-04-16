a58990ac8c2e61dd43ed6385cd967f51a7b41582
JorshBG <jordiyair29@gmail.com>
2023-04-16 14:38:18 -0600

    Forms created

---
+++ app/Http/Livewire/Forms/SignUp.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Forms;
+
+use Livewire\Component;
+
+class SignUp extends Component
+{
+    public function render()
+    {
+        return view('livewire.forms.sign-up');
+    }
+}
