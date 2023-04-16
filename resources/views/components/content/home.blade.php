27e5936926e52a95db22a5926fea573c21f03c90
JorshBG <jordiyair29@gmail.com>
2023-04-16 13:59:09 -0600

    Content of dashboard views

---
+++ resources/views/components/content/home.blade.php
@@ -0,0 +1,13 @@
+<x-partials.menu-bar parent="Categorias" child="Productos" />
+
+<div id="mainContent">
+
+    <div class="container-fluid h-fluid">
+        <div class="d-grid h-full center-grid">
+            <h1 class="display-1">
+                <span class="text-dark">¡Bienvenido!</span>
+            </h1>
+        </div>
+    </div>
+
+</div>
