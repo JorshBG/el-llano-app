62fcfd415663db213a84a9a3a475b887311b892f
JorshBG <jordiyair29@gmail.com>
2023-04-18 05:10:54 -0600

    fix migrations

--- database/migrations/2023_04_17_021733_create_buy_unite_table.php
+++ database/migrations/2023_04_17_021733_create_buy_unite_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('buy_unite');
+        Schema::dropIfExists('buy_unites');
     }
 };
